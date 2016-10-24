960ac60b31b74157308a8da065f85357672c4bbd
Pierre Karpov <push.default>
2016-10-24 02:24:48 -0700

    Lab finished

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,42 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e) {
+	//console.log("Project clicked");
+	e.preventDefault();
+	var color = $( this ).css( "background-color" );
+	if (color == 'rgb(127, 255, 0)') {
+		$(this).css("background-color", "");
+	} else {
+		//console.log(color);
+		$(this).css("background-color", "#7fff00");
+	}
+	var projectTitle = $(this).find("p").text();
+	var jumbotronHeader = $(".jumbotron h1");
+	jumbotronHeader.text(projectTitle);
+
+	var containingProject = $(this).closest(".project");
+	var image = $(containingProject).find("img");
+	if (image.length == 0) {
+		console.log("no img");
+	} else {
+		$(image).toggle();
+	}
+	/*
+	var description = $(containingProject).find(".project-description");
+	if (description.length == 0) {
+		containingProject.append("<div class='project-description'><p>Description of the project</p></div>");
+	} else {
+		description.html("<p>Stop clicking on me! You just did it at " + (new Date()) +  "</>");
+	}
+	*/
 }
